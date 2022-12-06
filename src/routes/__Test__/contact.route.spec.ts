af9b2850b280ed4f2d85ee3abb50fd1749c941ad
Niklas Andersson <niklas.andersson@iths.se>
2022-12-06 16:34:57 +0100

    Add tests to be implemented

--- src/routes/__Test__/contact.route.spec.ts
+++ src/routes/__Test__/contact.route.spec.ts
@@ -3,25 +3,77 @@ import makeApp from '../../app'
 
 const app = makeApp()
 
+const validContacts = [
+  {
+    "id": "638cfd06f84b41a7be61ebad",
+    "firstname": "Anna",
+    "lastname": "Andersson",
+    "email": "anna.andersson@gmail.com",
+    "personalnumber": "550713-1405",
+    "address": "Utvecklargatan 12",
+    "zipCode": "111 22",
+    "city": "Stockholm",
+    "country": "Sweden",
+    "lat": 59.3251172,
+    "lng": 18.0710935
+  },
+  {
+    "id": "638cfd06f84b41a7be61eadb",
+    "firstname": "Erik",
+    "lastname": "Eriksson",
+    "email": "erik.eriksson@gmail.com",
+    "personalnumber": "740301-1405",
+    "address": "Utvecklargatan 12",
+    "zipCode": "111 22",
+    "city": "Stockholm",
+    "country": "Sweden",
+    "lat": 59.3251172,
+    "lng": 18.0710935
+  },
+]
+
 describe('Testing Contact Route', () => {
 
+  describe('POST /contacts', () => {
+    it('should respond with error messages if incorrect data is posted - statuscode 400', () => {
+
+    })
+
+    it('should return posted user with statuscode 201', () => {
+
+    })
+
+  })
+
   describe('GET /contacts', () => {
-    it('should respond with 200 statuscode and array of contacts', async () => {
+    it('should GET /contacts and respond with 200 statuscode and array of max 10 contacts', async () => {
       const res = await request(app)
                           .get('/contacts')
                           .expect(200)
                           .expect('Content-Type', /json/)
 
-      expect(res.body.data).toStrictEqual([{
-        "firstname": "Anna",
-        "lastname": "Andersson",
-        "email": "anna.andersson@gmail.com",
-        "personalnumber": "550713-1405",
-        "address": "Utvecklargatan 12",
-        "zipCode": "111 22",
-        "city": "Stockholm",
-        "country": "Sweden"
-      }])
+      expect(res.body.data).toEqual(validContacts)
+      expect(res.body.data.length).toBeLessThanOrEqual(10)
+    })
+
+  })
+
+  describe('Get /contacts/:id', () => {
+    it('should GET /contact by id with status code 200', async () => {
+      const res = await request(app)
+                          .get('/contacts/638cfd06f84b41a7be61eadb')
+                          .expect(200)
+                          .expect('Content-Type', /json/)
+
+      expect(res.body.data).toEqual(validContacts[1])
+    })
+
+    it('should throw an error when not sending a correct mongo object id', () => {
+
+    })
+
+    it('should send back 404 statuscode when no contact found', () => {
+
     })
   })
 })
--- src/routes/contacts.ts
+++ src/routes/contacts.ts
@@ -3,16 +3,7 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/contacts', (req, res) => {
-  res.status(200).json({data: [{
-    "firstname": "Anna",
-    "lastname": "Andersson",
-    "email": "anna.andersson@gmail.com",
-    "personalnumber": "550713-1405",
-    "address": "Utvecklargatan 12",
-    "zipCode": "111 22",
-    "city": "Stockholm",
-    "country": "Sweden"
-  }]})
+  res.status(200).json({})
 })
 
 export default router
