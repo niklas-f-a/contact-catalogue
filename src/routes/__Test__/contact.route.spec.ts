1a98429e9cdae32137b5332bf3a2c0170ea61e94
Niklas Andersson <niklas.andersson@iths.se>
2022-12-06 15:42:12 +0100

    Add test for GET /contacts

---
+++ src/routes/__Test__/contact.route.spec.ts
@@ -0,0 +1,27 @@
+import { default as request } from 'supertest'
+import makeApp from '../../app'
+
+const app = makeApp()
+
+describe('Testing Contact Route', () => {
+
+  describe('/GET', () => {
+    it('should respond with 200 statuscode and array of contacts', async () => {
+      const res = await request(app)
+                          .get('/contacts')
+                          .expect(200)
+                          .expect('Content-Type', /json/)
+
+      expect(res).toStrictEqual([{
+        "firstname": "Anna",
+        "lastname": "Andersson",
+        "email": "anna.andersson@gmail.com",
+        "personalnumber": "550713-1405",
+        "address": "Utvecklargatan 12",
+        "zipCode": "111 22",
+        "city": "Stockholm",
+        "country": "Sweden"
+      }])
+    })
+  })
+})
