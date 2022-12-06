a20912edb831138a425c78212e0c24868a7d89da
Niklas Andersson <niklas.andersson@iths.se>
2022-12-06 16:44:23 +0100

    Add testts todo

---
+++ src/util/__Test__/validator.spec.ts
@@ -0,0 +1,39 @@
+import Validate from '../validate'
+
+describe('Validate', () => {
+  describe('validateEmail', () => {
+    it('should return error message with no valid email', () => {
+
+    })
+    it('should have no error message with correct email', () => {
+
+    })
+
+  })
+  describe('validateZipCode', () => {
+    it('should return error message with invalid zip code', () => {
+
+    })
+    it('should have no error message with correct zip code', () => {
+
+    })
+  })
+
+  describe('validatePersonalNumber', () => {
+    it('should return error message with invalid personal number', () => {
+
+    })
+    it('should have no error message with correct personal number', () => {
+
+    })
+  })
+
+  describe('validateText', () => {
+    it('should return error message with invalid text', () => {
+
+    })
+    it('should have no error message with correct text', () => {
+
+    })
+  })
+})
