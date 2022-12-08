51d10477ca364910dfb48e962cbbf7d220de6e24
Niklas Andersson <niklas.andersson@iths.se>
2022-12-08 20:02:30 +0100

    Add getAllContacts

--- src/db/models/contactService.ts
+++ src/db/models/contactService.ts
@@ -4,3 +4,7 @@ import Contact from "./contacts";
 export const createContact = async (newContact: Contact) => {
   await Contact.create(newContact)
 }
+
+export const getAllContacts = async (page: number, limit: number) => {
+  await Contact.find().limit(limit).skip(page)
+}
